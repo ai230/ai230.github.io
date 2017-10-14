88c8d802f9cceb18904e785864cebb744b7080e6
AiYamamoto <ay36230@gmail.com>
2017-10-14 13:28:43 -0700

    Changed footer font color

--- js/instagram.js
+++ js/instagram.js
@@ -1,65 +1,78 @@
 var token;
-$(function() {
-    token = window.location.hash;
-    if (token.substr(0, 14) == "#access_token=") {
-      showLogout();
-      getMyinfo();
-    } else {
-        document.getElementById("noTaken").innerHTML = "please login";
-        showLogin();
-    }
+var myToken;
+$(function () {
+
+    var test = JSON.parse('<?php echo $myJson; ?>');
+    //    token = window.location.hash;
+    myToken = "4158914349.610596b.e2eb2ea2076f4793ae79b49aa0bc0187";
+
+    //    if (token.substr(0, 14) == "#access_token=") {
+    //        showLogout();
+    //        //      getMyinfo();
+    //    } else {
+    //        document.getElementById("noTaken").innerHTML = "please login";
+    //        showLogin();
+    //    }
+    getMyinfo();
 });
-function showLogin(){
-  $('#logIn').show();
-  $('#logOut').hide();
+
+function showLogin() {
+    $('#logIn').show();
+    $('#logOut').hide();
 }
-function showLogout(){
-  $('#logOut').show();
-  $('#logIn').hide();
+
+function showLogout() {
+    $('#logOut').show();
+    $('#logIn').hide();
 }
-function getMyinfo(){
-  //users/self
-  $.ajax({
-      url: "https://api.instagram.com/v1/users/self/?access_token=" + token.substr(14),
-      method: "get",
-      dataType: "JSONP",
-      success: function(responce) {
-          var myName = responce.data["full_name"];
-          var myPic = responce.data["profile_picture"];
-          var myFollows = responce.data.counts["follows"];
-          var myFollowedBy = responce.data.counts["followed_by"];
-          $("#mypic").append("<img src='" + myPic + "'>");
-          $("#myname").append("<p>" + myName + "</p>");
-          $("#myfollows").append("<p>" + myFollows + " following</p>");
-          $("#myfollowedby").append("<p>" + myFollowedBy + " followers</p>");
-      },
-  });
-  //GET/users/self/follows
-  $.ajax({
-      url: "https://api.instagram.com/v1/users/self/followed-by?access_token=" + token.substr(14),
-      method: "get",
-      dataType: "JSONP",
-      success: function(responce) {
-          $("#followsInfo").append("<h3>Followed By</h3>");
-          for (var i = 0; i < responce.data.length; i++) {
-              var followedName = responce.data[i]["full_name"];
-              var followedUserName = responce.data[i]["username"];
-              var followedPic = responce.data[i]["profile_picture"];
-              var followedId = responce.data[i]["id"];
-              $("#followsInfo").append("<div class='hvr-bubble-float-bottom'><li><p>" + followedName + "</p><p>" + followedUserName + "</p><img onclick='getImage(" + followedId + ")' src='" + followedPic + "'></li></div>");
-          }
-      },
-  });
+
+function getMyinfo() {
+    //users/self
+    $.ajax({
+        //        url: "https://api.instagram.com/v1/users/self/?access_token=" + token.substr(14),
+        url: "https://api.instagram.com/v1/users/self/?access_token=" + myToken,
+        method: "get",
+        dataType: "JSONP",
+        success: function (responce) {
+            var myName = responce.data["full_name"];
+            var myPic = responce.data["profile_picture"];
+            var myFollows = responce.data.counts["follows"];
+            var myFollowedBy = responce.data.counts["followed_by"];
+            $("#mypic").append("<img src='" + myPic + "'>");
+            $("#myname").append("<p>" + myName + "</p>");
+            $("#myfollows").append("<p>" + myFollows + " following</p>");
+            $("#myfollowedby").append("<p>" + myFollowedBy + " followers</p>");
+        },
+    });
+    //GET/users/self/follows
+    $.ajax({
+        //        url: "https://api.instagram.com/v1/users/self/followed-by?access_token=" + token.substr(14),
+        url: "https://api.instagram.com/v1/users/self/followed-by?access_token=" + myToken,
+        method: "get",
+        dataType: "JSONP",
+        success: function (responce) {
+            $("#followsInfo").append("<h3>Followed By</h3>");
+            for (var i = 0; i < responce.data.length; i++) {
+                var followedName = responce.data[i]["full_name"];
+                var followedUserName = responce.data[i]["username"];
+                var followedPic = responce.data[i]["profile_picture"];
+                var followedId = responce.data[i]["id"];
+                $("#followsInfo").append("<div class='hvr-bubble-float-bottom'><li><p>" + followedName + "</p><p>" + followedUserName + "</p><img onclick='getImage(" + followedId + ")' src='" + followedPic + "'></li></div>");
+            }
+        },
+    });
 }
-function getImage(followedId){
+
+function getImage(followedId) {
     $('#followsInfoPics1').empty();
     $('#followsInfoPics2').empty();
     //GET/users/self/media/recent
     $.ajax({
-        url: "https://api.instagram.com/v1/users/" + followedId + "/media/recent/?access_token=" + token.substr(14),
+        //        url: "https://api.instagram.com/v1/users/" + followedId + "/media/recent/?access_token=" + token.substr(14),
+        url: "https://api.instagram.com/v1/users/" + followedId + "/media/recent/?access_token=" + myToken,
         method: "get",
         dataType: "JSONP",
-        success: function(responce) {
+        success: function (responce) {
             if (responce.data == undefined || responce.data.length == 0) {
                 $("#followsInfoPics2").append("No data found");
             } else {
